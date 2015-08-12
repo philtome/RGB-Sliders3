cf006b43d1fc7d1efdeb57300d6c06b1886a574f
LusahAir13 <LusahAir13@LusahAir13s-MacBook-Air.local>
2015-08-12 08:17:05 -0600

    this should be a working version of the project

--- RGB2/ViewController.swift
+++ RGB2/ViewController.swift
@@ -1,25 +1,41 @@
 //
 //  ViewController.swift
-//  RGB2
+//  RGB Sliders
 //
-//  Created by LusahAir13 on 8/12/15.
+//  Created by LusahAir13 on 8/11/15.
 //  Copyright (c) 2015 Lusah. All rights reserved.
 //
+// Commet added
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBackgroundColor()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func updateBackgroundColor() {
+        
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
 
-
+    
 }
 
